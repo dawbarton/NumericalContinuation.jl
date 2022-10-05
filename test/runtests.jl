c6646cb7816fc5bbb62122e0a2ce36df891c4bef
David AW Barton <david.barton@bristol.ac.uk>
2022-10-05 14:47:51 +0100

    Add Aqua.jl for auto testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using NumericalContinuation
 using Test
-
 using TestItemRunner
+
+@testitem "Aqua.jl tests" begin
+    using Aqua: Aqua
+    # ComponentArrays has ambiguities...
+    Aqua.test_all(NumericalContinuation; ambiguities=false)
+end
 
 @run_package_tests
