46ceefbadf2bdbf7b085c33c0f87ced82a3fe7a0
David AW Barton <david.barton@bristol.ac.uk>
2023-04-29 23:17:36 +0100

    Working TreeVector

--- src/treevector.jl
+++ src/treevector.jl
@@ -1,46 +1,137 @@
-mutable struct TreeVectorAxes{N, NAME, LEAF}
-    size::NTuple{N, Int}
-    index::NTuple{N, UnitRange{Int}}
-    leaf::LEAF
+mutable struct TreeVectorAxes{NAMES, LEAVES}
+    size::Int
+    index::UnitRange{Int}
+    leaves::LEAVES
 end
-TreeVectorAxes() = TreeVectorAxes{0, (), Tuple{}}((), (), ())
 
-function TreeVectorAxes(leaves, idx = 1)
-    name = Vector{Symbol}()
-    size = Vector{Int}()
-    index = Vector{UnitRange{Int}}()
-    leaf = Vector{Any}()
-    for (n, l) in named_pairs(leaves)
-        push!(name, n)
-        if l isa Integer
-            len = l
-            push!(leaf, ())
-        elseif l isa AbstractVector
-            len = length(l)
-            push!(leaf, ())
+function TreeVectorAxes(tree, idx = 1)
+    names = Vector{Symbol}()
+    size = 0
+    leaves = Vector{Any}()
+    firstidx = idx
+    for (name, leaf) in named_pairs(tree)
+        push!(names, name)
+        if leaf isa Number
+            throw(ArgumentError("Leaves of a tree vector should be vectors"))
+        elseif leaf isa AbstractVector
+            len = length(leaf)
+            push!(leaves, TreeVectorAxes{(),Tuple{}}(len, idx:(idx+len-1), ()))
         else
-            ll = TreeVectorAxes(l, idx)
-            len = length(ll)
-            push!(leaf, ll)
+            treeleaf = TreeVectorAxes(leaf, idx)
+            len = length(treeleaf)
+            push!(leaves, treeleaf)
         end
-        push!(size, len)
-        push!(index, idx:(idx + len - 1))
+        size += len
         idx += len
     end
-    _N = length(name)
-    _NAME = (name...,)
-    _size = (size...,)
-    _index = (index...,)
-    _leaf = (leaf...,)
-    _LEAF = typeof(_leaf)
-    TreeVectorAxes{_N, _NAME, _LEAF}(_size, _index, _leaf)
+    _NAME = (names...,)
+    _leaves = (leaves...,)
+    _LEAF = typeof(_leaves)
+    TreeVectorAxes{_NAME, _LEAF}(size, firstidx:(idx - 1), _leaves)
 end
 
 Base.isempty(::TreeVectorAxes) = false
 Base.isempty(::TreeVectorAxes{0}) = true
-Base.length(x::TreeVectorAxes) = sum(x.size)
+Base.length(ax::TreeVectorAxes) = getfield(ax, :size)
+Base.keys(::TreeVectorAxes{NAMES}) where NAMES = NAMES
 
-named_pairs(x::NamedTuple) = pairs(x)
-named_pairs(x::Vector{<:Pair{Symbol}}) = x
+named_pairs(ax::NamedTuple) = pairs(ax)
+named_pairs(ax::Vector{<:Pair{Symbol}}) = ax
+# TODO: named_pairs for TreeVectors
 
-@inline Base.getproperty(x::TreeVectorAxes, leaf::Symbol) = _get_leaf(x, Val(leaf))
+@inline Base.getproperty(ax::TreeVectorAxes, leaf::Symbol) = _get_leaf(ax, Val(leaf))
+
+function _gen_get_leaf(::Type{TreeVectorAxes{NAME, LEAVES}}, ::Type{Val{LEAF}}) where {NAME, LEAVES, LEAF}
+    idx = findfirst(==(LEAF), NAME)
+    if idx === nothing
+        return :(error("Unknown field: ", $LEAF))
+    else
+        return :(getindex(getfield(ax, :leaves), $idx))
+    end
+end
+
+@generated function _get_leaf(ax::TreeVectorAxes, leaf)
+    return _gen_get_leaf(ax, leaf)
+end
+
+function TreeVectorType(tree)
+    T = Union{}
+    for (name, leaf) in named_pairs(tree)
+        if leaf isa AbstractVector
+            T = promote_type(T, eltype(leaf))
+        else
+            T = promote_type(T, TreeVectorType(leaf))
+        end
+    end
+    return T
+end
+
+struct TreeVector{T, V, A <: TreeVectorAxes} <: AbstractVector{T}
+    data::V
+    axes::A
+end
+
+function TreeVector(tree)
+    axes = TreeVectorAxes(tree)
+    T = TreeVectorType(tree)
+    data = Vector{T}(undef, length(axes))
+    tv = TreeVector{T, Vector{T}, typeof(axes)}(data, axes)
+    copyto!(tv, tree)
+end
+
+function Base.copyto!(dest::TreeVector, tree)
+    for (name, leaf) in named_pairs(tree)
+        copyto!(getproperty(dest, name), leaf)
+    end
+    return dest
+end
+
+function Base.copyto!(dest::TreeVector, src::AbstractVector)
+    copyto!(vec(dest), src)
+    return dest
+end
+
+function Base.copyto!(::AbstractVector, ::Number)
+    throw(ArgumentError("Cannot copy a number to a vector"))
+end
+
+function Base.copyto!(dest::TreeVector, bc::Base.Broadcast.Broadcasted)
+    copyto!(vec(dest), bc)
+end
+
+function Base.getproperty(A::TreeVector, leaf::Symbol)
+    ax = getproperty(getfield(A, :axes), leaf)
+    return TreeVector{eltype(A), Vector{eltype(A)}, typeof(ax)}(getfield(A, :data), ax)
+end
+
+function Base.setproperty!(::TreeVector, ::Any, ::Symbol)
+    throw(ArgumentError("Cannot change the structure of a TreeVector"))
+end
+
+Base.vec(A::TreeVector) = view(getfield(A, :data), getfield(getfield(A, :axes), :index))
+Base.size(A::TreeVector) = (length(A),)
+Base.length(A::TreeVector) = length(getfield(A, :axes))
+Base.eltype(::TreeVector{T}) where {T} = T
+Base.getindex(A::TreeVector, args...) = getindex(vec(A), args...)
+Base.setindex!(A::TreeVector, v, args...) = setindex!(vec(A), v, args...)
+Base.firstindex(A::TreeVector) = firstindex(vec(A))
+Base.lastindex(A::TreeVector) = lastindex(vec(A))
+Base.IndexStyle(::Type{<:TreeVector{T, V}}) where {T, V} = IndexStyle(V)
+Base.iterate(A::TreeVector, state = firstindex(A)) = iterate(vec(A), state)
+
+function Base.similar(A::TreeVector, ::Type{S}, dims::Dims)
+    if dims == size(A)
+        ax = getfield(A, :axes)
+        return TreeVector{S, Vector{S}, typeof(ax)}(Vector{S}(undef, length(A)), ax)
+    else
+        throw(ArgumentError("Cannot change the structure of a TreeVector"))
+    end
+end
+
+Base.keys(A::TreeVector) = keys(getfield(A, :axes))
+
+# TODO: Implement Base.pairs for TreeVectors
+
+TreeVectorAxes(A::TreeVector) = getfield(A, :axes)
+
+# TODO: Implement arithmetic that preserves the tree structure
